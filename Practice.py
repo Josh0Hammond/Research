57d77bd7a2a63c9efaf4f0ad9a104a0348f179d5
Josh0Hammond <167526957+Josh0Hammond@users.noreply.github.com>
2024-04-19 23:10:21 +1200

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,22 @@
+#W3 Schools Python#
+class Person:
+  def __init__(mysillyobject, name, age):
+    mysillyobject.name = name
+    mysillyobject.age = age
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+#Generate new password#
+    def desc = input("Password Change"):
+        generatePassword():
+        password = ""
+        count = 0
+        while count < PasswordLength:
+            password += random.choice(passwordChars)
+            count += 1
+            print("New Password: ", password)
+            generatePassword()
